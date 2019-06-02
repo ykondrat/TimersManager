69047e85127848cd207313b21f5b907f52acd13c
Yevhen Kondratyev <yevhenkondratyev@Yevhens-MacBook-Pro.local>
2019-06-02 23:50:39 +0300

    finish first step

---
+++ index-1.js
@@ -0,0 +1,140 @@
+class TimersManager {
+    constructor () {
+        this.timers = [];
+        this.requiredProperties = ['name', 'delay', 'interval', 'job'];
+    }
+
+    checkTimer (timer) {
+        const existedTimersName = this.timers.map((item) => item.name);
+        const arrayOfTimerKeys = Object.keys(timer);
+
+        // Check if type of timer is object
+        if (typeof timer !== 'object') {
+            throw new Error(
+                'timer must be an object:\n{\n' +
+                    '\tname*:     String, // timer name\n' +
+                    '\tdelay*:    Number,  // timer delay in ms\n' +
+                    '\tinterval*: Boolean, // is timer or interval\n' +
+                    '\tjob*:      Function, // timer job\n' +
+                '}\n'
+            );
+        }
+
+        // Check all properties of timer
+        for (const key in timer) {
+            if (timer.hasOwnProperty(key)) {
+                if (key === 'name' && typeof timer[key] !== 'string') {
+                    throw new Error('name must be a string');
+                } else if (key === 'delay' && (typeof timer[key] !== 'number' || (timer[key] < 0 || timer[key] > 5000))) {
+                    throw new Error('delay must be a number and cannot be less than 0 and greater than 5000')
+                } else if (key === 'interval' && typeof timer[key] !== 'boolean' ) {
+                    throw new Error('interval must be a boolean');
+                } else if (key === 'job' && typeof timer[key] !== 'function') {
+                    throw new Error('job must be a function');
+                }
+            }
+        }
+
+        // Check if all properties of timer is set
+        for (let i = 0; i < this.requiredProperties.length; i++) {
+            if (!arrayOfTimerKeys.includes(this.requiredProperties[i])) {
+                throw new Error(`Property ${this.requiredProperties[i]} is required`);
+            }
+        }
+
+        // Check if timer name is uniq
+        if (existedTimersName.includes(timer.name)) {
+            throw new Error('Such timer name already exist');
+        }
+    }
+
+    clearTask (index) {
+        if (this.timers[index].interval) {
+            clearInterval(this.timers[index].task);
+            this.timers[index].task = null;
+        } else {
+            clearTimeout(this.timers[index].task);
+            this.timers[index].task = null;
+        }
+    }
+
+    startTask (index) {
+        if (this.timers[index].interval) {
+            this.timers[index].task = setInterval(() => {
+                this.timers[index].job(...this.timers[index].params);
+            }, this.timers[index].delay);
+        } else {
+            this.timers[index].task = setTimeout(() => {
+                this.timers[index].job(...this.timers[index].params);
+            }, this.timers[index].delay);
+        }
+    }
+
+    add (timer, ...props) {
+        this.checkTimer(timer);
+        timer.params = props;
+        this.timers.push(timer);
+
+        return this;
+    }
+
+    remove (timerName) {
+        const index = this.pause(timerName);
+
+        if (index >= 0) {
+            this.timers.splice(index, 1);
+        }
+
+        return this;
+    }
+
+    start () {
+        for (let i = 0; i < this.timers.length; i++) {
+            if (!this.timers[i].task) {
+                this.startTask(i);
+            }
+        }
+    }
+
+    stop () {
+        for (let i = 0; i < this.timers.length; i++) {
+            if (this.timers[i].task) {
+                this.clearTask(i);
+            }
+        }
+    }
+
+    pause (timerName) {
+        if (typeof timerName === 'string') {
+            for (let i = 0; i < this.timers.length; i++) {
+                if (this.timers[i].name === timerName) {
+                    if (!this.timers[i].task) {
+                        return null;
+                    }
+                    this.clearTask(i);
+                    return i;
+                }
+            }
+            return -1;
+        } else {
+            throw new Error('Timer name must be a string');
+        }
+    }
+
+    resume (timerName) {
+        if (typeof timerName === 'string') {
+            for (let i = 0; i < this.timers.length; i++) {
+                if (this.timers[i].name === timerName) {
+                    if (this.timers[i].task) {
+                        return null;
+                    }
+                    this.startTask(i);
+                    return i;
+                }
+            }
+            return -1;
+        } else {
+            throw new Error('Timer name must be a string');
+        }
+    }
+}
